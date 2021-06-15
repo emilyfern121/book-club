24d428ce4d47c75d9f60e59d4fc683f64475a87f
Nate Jones <15nate.jones@gmail.com>
2021-06-14 21:26:47 -0400

    Updated my routes to serve a homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,7 +4,8 @@ const { User } = require('../models/User');
 
 router.get('/', async (req,res) => {
     try {
-        
+        //Placeholder for homepage render via handlebars
+        res.render('homepage');
     } catch (err) {
         res.status(500).json(err);
     }
