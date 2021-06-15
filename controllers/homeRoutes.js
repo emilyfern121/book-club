5092e5a915ea2ddd02025a261e8b9e1f3ca226a2
Nate Jones <15nate.jones@gmail.com>
2021-06-14 20:34:57 -0400

    Created Controllers

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,11 @@
+//import dependencies
+const router = require('express').Router();
+const { User } = require('../models/User');
+
+router.get('/', async (req,res) => {
+    try {
+        
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
