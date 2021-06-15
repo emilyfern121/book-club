8649863ae22e9390410276768ab37e57b12e8e23
Emily Fernandez <emilyfern121@gmail.com>
2021-06-14 21:11:52 -0400

    modified the seed file

--- seeds/index.js
+++ seeds/index.js
@@ -1,17 +1,26 @@
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+const { User, Book } = require('../models');
 
 const userData = require('./userData.json');
+const projectData = require('./bookData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  await User.bulkCreate(userData, {
+  const users = await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
+
+  for (const book of bookData) {
+    await Book.create({
+      ...book,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
 
   process.exit(0);
 };
 
 seedDatabase();
+
