d43908b495a38b6fe8d4b22edcbf844377028b6b
Nate Jones <15nate.jones@gmail.com>
2021-06-16 19:19:03 -0400

    Updating seeds folder

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -5,7 +5,14 @@ const { User } = require('../../models');
 router.post('/', async (req, res) =>{
     try {
         const userData = await User.create(req.body);
-    } catch (error) {
+        req.session.save(() => {
+            req.session.user_id = userData.isSoftDeleted;
+            req.session.logged_in = true;
         
+            res.status(200).json(userData);
+        });//end save function
+        
+    } catch (err) {
+        res.status(400).json(err);
     }
-})
+});
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,12 +1,21 @@
 //import dependencies
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, Book } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req,res) => {
     try {
         const dbUserData = await User.findAll();
         //Placeholder for homepage render via handlebars
         res.render('homepage', { dbUserData });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/book/:id', async (req,res) => {
+    try {
+        const bookData = await Book.findByPk(req.params.id);
     } catch (err) {
         res.status(500).json(err);
     }
