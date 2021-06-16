92e07fd44dd44d392ac2a3869c867f48b5eaf5b6
Emily Fernandez <emilyfern121@gmail.com>
2021-06-15 20:34:06 -0400

    modified the bookData and userData js files in the seed folder

--- seeds/bookData.js
+++ seeds/bookData.js
@@ -2,16 +2,19 @@ const { Book } = require('../models')
 
 const bookdata = [
     {
-      "title": "The Great Gatsby",
-      "author": "F. Scott Fitzgerald"
+      title: "The Great Gatsby",
+      author: "F. Scott Fitzgerald",
+      ibsn: "",
     },
     {
-      "title": "Moby Dick",
-      "author": "Herman Melville"
+      title: "Moby Dick",
+      author: "Herman Melville",
+      ibsn: "",
     },
     {
-      "title": "The Odyssey",
-      "author": "Homer"
+      title: "The Odyssey",
+      author: "Homer",
+      ibsn: "",
     }
   ]
 
